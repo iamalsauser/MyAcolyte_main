92b282edb886f4a3a9156100b5206fc63e157838
Iamalsauser <parthsinhv4@gmail.com>
2025-03-07 15:59:12 +0530

    Initial Commit

---
+++ MyAcolyte/MyAcolyteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAcolyteApp.swift
+//  MyAcolyte
+//
+//  Created by admin17 on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAcolyteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
